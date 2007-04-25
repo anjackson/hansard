38c63d00dbe38842c731bcfa0f58429026f54562
Louise Crow <louise.crow@gmail.com>
2007-04-25 13:48:32 +0000

    Added new relationships

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,5 @@
 class Section < ActiveRecord::Base
+  belongs_to :sitting, :foreign_key => "SittingId"
+  set_primary_key :Id
+
 end
--- app/models/sitting.rb
+++ app/models/sitting.rb
@@ -1,2 +1,9 @@
 class Sitting < ActiveRecord::Base
+  has_many :sections, :foreign_key => "SittingId"
+  set_primary_key :Id
+  
+  def Date
+    @Date = self.SatAt.strftime("%A %d %B %Y")
+  end
+  
 end
