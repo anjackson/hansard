a8ef412ad3e3f3346521c659ef2c1c11231ed31d
Louise Crow <louise.crow@gmail.com>
2007-08-10 09:56:26 +0000

    Resolved conflict

--- spec/views/oral_question_sections/show.xml_spec.rb
+++ spec/views/oral_question_sections/show.xml_spec.rb
@@ -7,19 +7,7 @@ describe "/oral_question_sections/show.rxml" do
     @oral_question_section = mock_model(OralQuestionSection)
     assigns[:oral_question_section] = @oral_question_section
   end
-<<<<<<< .mine
 
-  it "should render the 'oral_question_section' template once" do 
-    @controller.template.should_receive(:render).and_return(nil)
-    render "/oral_question_sections/show.rxml"
-  end
-=======
-  # 
-  # it "should render the 'oral_question_section' partial once" do 
-  #   @controller.template.should_receive(:render).with(:partial => "oral_question_section").and_return(nil)
-  #   render "/oral_question_sections/show.rxml"
-  # end
->>>>>>> .r1458
-  
+
 end
 
