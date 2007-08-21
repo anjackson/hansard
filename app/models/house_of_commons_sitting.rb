2d0178e1f99e5c5bbec2fb5b26ba325c206b35b0
Louise Crow <louise.crow@gmail.com>
2007-08-21 15:35:04 +0000

    Tweaks to xml whitespacing

--- app/models/house_of_commons_sitting.rb
+++ app/models/house_of_commons_sitting.rb
@@ -4,13 +4,13 @@ class HouseOfCommonsSitting < Sitting
   alias :to_activerecord_xml :to_xml
   
   def to_xml(options={})
-    xml = options[:builder] ||= Builder::XmlMarkup.new
+    xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => 1)
     xml.housecommons do
       xml.image(:src => start_image_src)
       xml.col(start_column)
       xml.title(title)
       xml.date(date_text, :format => date.strftime("%Y-%m-%d"))
-      xml << text
+      xml << text + "\n"
       debates.to_xml(options) if debates
     end
   end
