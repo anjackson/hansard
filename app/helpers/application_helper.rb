b81cab30e08437004fa73517f99579cec814d4d4
Rob McKinnon <rob@theyworkforyou.co.nz>
2007-08-21 11:32:15 +0000

    improved formatting of member contribution, eg changed colon location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,27 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def colon_after_member_name contribution
+    if (!contribution.member_constituency and !contribution.procedural_note)
+      ':'
+    else
+      ''
+    end
+  end
+
+  def colon_after_constituency contribution
+    if (contribution.member_constituency and !contribution.procedural_note)
+      ':'
+    else
+      ''
+    end
+  end
+
+  def format_member_contribution(text)
+    if text.include? ':'
+      text.sub(':','').strip
+    else
+      text
+    end
+  end
 end
--- spec/views/commons/_contribution_partial_spec.rb
+++ spec/views/commons/_contribution_partial_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe '_contribution partial', 'when passed quote contribution' do
 
   before do
-    @text = 'That Sir Antony Buck and Mr. Robert Key be discharged from the Select Committee on the Armed Forces Bill and that Mr. Tony Baldry and Mr. Nicholas Soames be added to the Committee.&#x2014;<i>(Mr. Maude.]</i>'
+    @text = ': That Sir Antony Buck and Mr. Robert Key be discharged from the Select Committee on the Armed Forces Bill and that Mr. Tony Baldry and Mr. Nicholas Soames be added to the Committee.&#x2014;<i>(Mr. Maude.]</i>'
     @quote = mock_model(QuoteContribution)
     @quote.should_receive(:text).and_return @text
     @controller.template.stub!(:contribution).and_return @quote
@@ -25,7 +25,6 @@ describe '_contribution partial', 'when passed procedural contribution' do
     @text = '[MADAM SPEAKER <i>in the Chair</i>]'
 
     @procedural = mock_model(ProceduralContribution)
-    # @procedural.should_receive(:is_a?).with(ProceduralContribution).and_return(true)
     @procedural.should_receive(:text).and_return @text
     @controller.template.stub!(:contribution).and_return @procedural
 
@@ -38,7 +37,7 @@ describe '_contribution partial', 'when passed procedural contribution' do
 
 end
 
-describe '_section partial', 'when passed members contribution' do
+describe '_section partial', 'when passed members contribution with oral_question_no, constituency and procedural note' do
 
   # <p>1. <member>Mr. David Borrow <memberconstituency>(South Ribble)</memberconstituency></member><membercontribution>: What assessment he has made of the responses to the pensions Green Paper received to date. [68005]</membercontribution></p>
   before do
@@ -47,13 +46,13 @@ describe '_section partial', 'when passed members contribution' do
     @member = 'Mr. David Borrow'
     @member_constituency = '(South Ribble)'
     @oral_question_no = '1.'
-    @contribution_text = 'What assessment he has made of the responses to the pensions Green Paper received to date. [68005]'
+    @contribution_text = ': What assessment he has made of the responses to the pensions Green Paper received to date. [68005]'
 
-    @contribution.should_receive(:text).and_return '<p>'+@contribution_text+'</p>'
-    @contribution.should_receive(:member).and_return @member
-    @contribution.should_receive(:member_constituency).twice.and_return @member_constituency
-    @contribution.should_receive(:oral_question_no).twice.and_return @oral_question_no
-    @contribution.should_receive(:procedural_note).twice.and_return '<i>(seated and covered)</i>'
+    @contribution.stub!(:text).and_return @contribution_text
+    @contribution.stub!(:member).and_return @member
+    @contribution.stub!(:member_constituency).and_return @member_constituency
+    @contribution.stub!(:oral_question_no).and_return @oral_question_no
+    @contribution.stub!(:procedural_note).and_return '<i>(seated and covered)</i>'
 
     @controller.template.stub!(:contribution).and_return(@contribution)
 
@@ -65,8 +64,8 @@ describe '_section partial', 'when passed members contribution' do
       with_tag('span.oral_question_no', @oral_question_no)
       with_tag('cite.member', @member)
       with_tag('span.member_constituency', @member_constituency)
-      with_tag('span.procedural_note', '(seated and covered)')
-      with_tag('blockquote.contribution_text', @contribution_text)
+      with_tag('span.procedural_note', '(seated and covered):')
+      with_tag('blockquote.contribution_text', @contribution_text.sub(':','').strip)
     end
   end
 
@@ -74,8 +73,8 @@ describe '_section partial', 'when passed members contribution' do
     response.should have_tag('cite.member', @member)
   end
 
-  it 'should show a procedural note in a span with class "procedural_note"' do
-    response.should have_tag('span.procedural_note', '(seated and covered)')
+  it 'should show a procedural note and colon in a span with class "procedural_note"' do
+    response.should have_tag('span.procedural_note', '(seated and covered):')
   end
 
   it 'should show member constituency name in span with class "member_constituency"' do
@@ -87,7 +86,60 @@ describe '_section partial', 'when passed members contribution' do
   end
 
   it 'should show contribution text in blockquote with class "contribution_text"' do
-    response.should have_tag('blockquote.contribution_text', @contribution_text)
+    response.should have_tag('blockquote.contribution_text', @contribution_text.sub(':','').strip)
   end
 
+end
+
+describe '_section partial', 'when passed members contribution with constituency' do
+
+  before do
+    @contribution = mock_model(MemberContribution)
+    @member = 'Mr. David Borrow'
+    @member_constituency = '(South Ribble)'
+    @contribution_text = ': What assessment he has made of the responses to the pensions Green Paper received to date. [68005]'
+
+    @contribution.stub!(:text).and_return @contribution_text
+    @contribution.stub!(:member).and_return @member
+    @contribution.stub!(:member_constituency).and_return @member_constituency
+    @contribution.stub!(:oral_question_no).and_return nil
+    @contribution.stub!(:procedural_note).and_return nil
+
+    @controller.template.stub!(:contribution).and_return(@contribution)
+
+    render 'commons/_contribution.haml'
+  end
+
+  it 'should show member constituency name and colon in span with class "member_constituency"' do
+    response.should have_tag('span.member_constituency', @member_constituency+':')
+  end
+
+end
+
+describe '_section partial', 'when passed members contribution without constituency' do
+
+  before do
+    @contribution = mock_model(MemberContribution)
+    @member = 'Mr. David Borrow'
+    @member_constituency = '(South Ribble)'
+    @contribution_text = ': What assessment he has made of the responses to the pensions Green Paper received to date. [68005]'
+
+    @contribution.stub!(:text).and_return @contribution_text
+    @contribution.stub!(:member).and_return @member
+    @contribution.stub!(:member_constituency).and_return nil
+    @contribution.stub!(:oral_question_no).and_return nil
+    @contribution.stub!(:procedural_note).and_return nil
+
+    @controller.template.stub!(:contribution).and_return(@contribution)
+
+    render 'commons/_contribution.haml'
+  end
+
+  it 'should show member name and colon in cite with class "member"' do
+    response.should have_tag('cite.member', @member+':')
+  end
+
+  it 'should not show span with class "member_constituency"' do
+    response.should_not have_tag('span.member_constituency')
+  end
 end
