8cf81adee9f66b9640876594acb3effad584e041
Louise Crow <louise.crow@gmail.com>
2007-08-21 15:39:35 +0000

    Moving to_xml to appropriate class level

--- app/models/member_contribution.rb
+++ app/models/member_contribution.rb
@@ -1,3 +1,24 @@
 class MemberContribution < Contribution
 
+  alias :to_activerecord_xml :to_xml
+
+  def member_contribution
+    text
+  end
+  
+  def to_xml(options={})
+    xml = options[:builder] ||= Builder::XmlMarkup.new
+    
+    xml.p(:id => xml_id) do
+      xml.text! oral_question_no || ""
+      xml.member do
+        xml << member.strip
+        xml.memberconstituency(member_constituency) if member_constituency
+      end
+      xml.membercontribution do
+        xml << text.strip if text
+      end
+    end
+  end
+
 end
