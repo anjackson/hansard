a92f609dc2d7ace4edac6989e55b26169e9dad90
Louise Crow <louise.crow@gmail.com>
2007-08-21 15:43:13 +0000

    consolidating specs in shared behaviours

--- spec/models/oral_questions_spec.rb
+++ spec/models/oral_questions_spec.rb
@@ -22,38 +22,19 @@ describe OralQuestions, ".to_xml" do
   
   before do
     @mock_builder = mock_oral_questions_builder
-    @oral_questions = OralQuestions.new
-  end
-  
-  it "should have an 'oralquestions' tag" do
-    @oral_questions.to_xml.should have_tag("oralquestions", :count => 1)
+    @section = OralQuestions.new
+    @subsection_class = OralQuestionsSection
   end
   
   it "should have one 'title' tag containing the title " do
-    @oral_questions.title = "test title"
-    @oral_questions.to_xml.should have_tag("title", :text => "test title", :count => 1)
+     @section.title = "test title"
+     @section.to_xml.should have_tag("title", :text => "test title", :count => 1)
+   end
+  
+  it "should have an 'oralquestions' tag" do
+    @section.to_xml.should have_tag("oralquestions", :count => 1)
   end
   
-  it "should call the to_xml method on each of it's contributions, passing it's xml builder" do
-    Builder::XmlMarkup.should_receive(:new).and_return(@mock_builder)
-    first_contribution = mock_model(Contribution)
-    second_contribution = mock_model(Contribution)
-    @oral_questions.contributions << first_contribution
-    @oral_questions.contributions << second_contribution
-    first_contribution.should_receive(:to_xml).with(:builder => @mock_builder)
-    second_contribution.should_receive(:to_xml).with(:builder => @mock_builder)
-    @oral_questions.to_xml
-  end
-   
-  it "should call the to_xml method on each of it's sections, passing it's xml builder" do
-    Builder::XmlMarkup.should_receive(:new).and_return(@mock_builder)
-    first_oral_questions = mock_model(OralQuestionsSection)
-    second_oral_questions = mock_model(OralQuestionsSection)
-    @oral_questions.sections << first_oral_questions
-    @oral_questions.sections << second_oral_questions
-    first_oral_questions.should_receive(:to_xml).with(:builder => @mock_builder)
-    second_oral_questions.should_receive(:to_xml).with(:builder => @mock_builder)
-    @oral_questions.to_xml
-  end
+  it_should_behave_like "a section to_xml method"
   
 end
