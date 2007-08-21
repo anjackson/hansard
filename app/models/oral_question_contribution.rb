24461d8a809b4545141420ab683296b47254dc6b
Louise Crow <louise.crow@gmail.com>
2007-08-21 15:31:02 +0000

    moving to_xml to superclass

--- app/models/oral_question_contribution.rb
+++ app/models/oral_question_contribution.rb
@@ -1,19 +1,5 @@
 class OralQuestionContribution < MemberContribution
 
   belongs_to :section, :class_name => "OralQuestionSection", :foreign_key => 'section_id'
-  alias :to_activerecord_xml :to_xml
-
-  def member_contribution
-    text
-  end
-  
-  def to_xml(options={})
-    xml = options[:builder] ||= Builder::XmlMarkup.new
-    xml.p(:id => xml_id) do
-      xml.text! oral_question_no || ""
-      xml.member member
-      xml.membercontribution text
-    end
-  end
 
 end
