31c0ddc6f12d92747e31992f0241b892705a41d0
Louise Crow <louise.crow@gmail.com>
2007-08-21 15:25:43 +0000

    Adding to_xml method for vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,15 @@
 class Vote < ActiveRecord::Base
 
   belongs_to :division
+  alias :to_activerecord_xml :to_xml
 
+  def to_xml(options={})
+    xml = options[:builder] ||= Builder::XmlMarkup.new
+    xml << name
+    if constituency
+      xml.i do
+        xml << "(#{constituency})"
+      end 
+    end
+  end
 end
