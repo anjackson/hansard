01e5e1eeb2b42b38c1469998efecf63bc91ae073
Louise Crow <louise.crow@gmail.com>
2007-08-21 15:42:44 +0000

    tweak to params

--- app/models/oral_questions.rb
+++ app/models/oral_questions.rb
@@ -2,7 +2,8 @@ class OralQuestions < Section
 
   has_many :sections, :class_name => "OralQuestionsSection", :foreign_key => 'parent_section_id'
 
-  def outer_tag xml
+  def outer_tag(options)
+    xml = options[:builder] ||= Builder::XmlMarkup.new
     xml.oralquestions do
       yield
     end
