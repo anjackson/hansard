cc1168966098001368fc1de84623f16f812c5a79
Robert Brook <mail@robertbrook.com>
2007-09-18 10:58:45 +0000

    Pedantry

--- spec/views/source_files/index.haml_spec.rb
+++ spec/views/source_files/index.haml_spec.rb
@@ -2,10 +2,10 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "source_files/index.haml", " in general" do
   
-  it "should have an 'h1' tag with the text 'Historical Hansard: Source Files'" do 
+  it "should have an 'h1' tag with the text 'Source Files'" do 
     assigns[:source_files] = []
     render 'source_files/index.haml'
-    response.should have_tag("h1", :text => "Historical Hansard: Source Files")
+    response.should have_tag("h1", :text => "Source Files")
   end
   
 end
