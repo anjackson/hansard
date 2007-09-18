2866be8ef902817a49a8a5f882e9819053921137
Robert Brook <mail@robertbrook.com>
2007-09-18 14:30:26 +0000

    trying to match regex of NUMBER + Source Files

--- spec/views/source_files/index.haml_spec.rb
+++ spec/views/source_files/index.haml_spec.rb
@@ -8,9 +8,15 @@ describe "source_files/index.haml", " in general" do
     response.should have_tag("h1")
   end
   
-  
+  #@expected_text_regex = /Source files/
+  #assigns[:source_files] = []
+  #render 'source_files/index.haml'
+  #response.should =~ @expected_text_regex
   it "should have an 'h1' tag with the text 'Source Files'" do 
 
+    
   end
   
 end
+
+
