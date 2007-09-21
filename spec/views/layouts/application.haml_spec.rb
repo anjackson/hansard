cfa6a39daca0517a17758c8411fedfa0e4a51126
Robert Brook <mail@robertbrook.com>
2007-09-21 14:08:38 +0000

    adding specs

--- spec/views/layouts/application.haml_spec.rb
+++ spec/views/layouts/application.haml_spec.rb
@@ -6,10 +6,16 @@ describe "application.haml", " in general" do
     render 'layouts/application.haml'
   end
   
+  it 'should render the HTML 5 doctype'
+  
   it 'should render the Google Custom Search box if we are online'
   
-  it 'should not attempt to render the Google Custom Search box if we are offline'
+  it 'should not render the Google Custom Search box if we are offline'
   
   it 'should render the del.icio.us box'
+  
+  it 'should should have a link rel="alternate" with appropriate title pointing to the xml source if on a day page'
+  
+  it 'should should have a link rel="alternate" with appropriate title pointing to the xml output if on a day page'
   
 end
