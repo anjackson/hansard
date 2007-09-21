1dbb9b3802f3215f51b37939108395efbf5fa942
Robert Brook <mail@robertbrook.com>
2007-09-21 13:54:53 +0000

    skeleton for testing layout application.haml

---
+++ spec/views/layouts/application.haml_spec.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "application.haml", " in general" do
+  
+  def do_render 
+    render 'layouts/aplication.haml'
+  end
+  
+  it 'should render the Google Custom Search box if we are online' do
+  end
+  
+  it 'should not attempt to render the Google Custom Search box if we are offline' do
+  end
+  
+end
