2ec6c92efddcc04bcf54cbb0a58f494614eecffd
Robert Brook <mail@robertbrook.com>
2007-09-21 14:00:41 +0000

    er, couldn't find that thing in the members code to timeout!

--- spec/views/layouts/application.haml_spec.rb
+++ spec/views/layouts/application.haml_spec.rb
@@ -6,10 +6,10 @@ describe "application.haml", " in general" do
     render 'layouts/application.haml'
   end
   
-  it 'should render the Google Custom Search box if we are online' do
-  end
+  it 'should render the Google Custom Search box if we are online'
   
-  it 'should not attempt to render the Google Custom Search box if we are offline' do
-  end
+  it 'should not attempt to render the Google Custom Search box if we are offline'
+  
+  it 'should render the del.icio.us box'
   
 end
