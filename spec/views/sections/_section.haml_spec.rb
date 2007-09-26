88bf7a1694ce3d82a39e27df11512235ee1eb1d0
Robert Brook <mail@robertbrook.com>
2007-09-26 15:22:53 +0000

    some specs

--- spec/views/sections/_section.haml_spec.rb
+++ spec/views/sections/_section.haml_spec.rb
@@ -4,6 +4,7 @@ describe "_section partial", " in general" do
 
   it "should ask the section assigned to it for it's marker html" do
     section = mock_model(Section)
+    section.stub!(:title).and_return("My fake title")
     section.stub!(:title_for_linking).and_return("transport_(parking)")
     section.stub!(:title_cleaned_up).and_return("")
     section.stub!(:sections).and_return([])
@@ -139,16 +140,37 @@ describe "_section_partial", " when passed an oral questions section with contri
     render 'sections/_section.haml'
   end
   
-  it "should show an introduction if there is one" do
+  it "should show an introduction if there is one and when a title is present" do
     @introduction = mock("intro model")
+    @oral_questions.stub!(:title).and_return("Some title")
     @introduction.stub!(:text).and_return("introduction text")
     @oral_questions.stub!(:introduction).and_return(@introduction)
     do_render
     response.should have_tag("p[class=question_introduction]", :text => "introduction text")
   end
   
-  it "should show the contributions" do
+  it "should show an introduction if there is one even when a title is present but has no text" do
+    @introduction = mock("intro model")
+    @oral_questions.stub!(:title).and_return("")
+    @introduction.stub!(:text).and_return("introduction text")
+    @oral_questions.stub!(:introduction).and_return(@introduction)
+    do_render
+    response.should have_tag("p[class=question_introduction]", :text => "introduction text")
+  end
+  
+  it "should show the contributions when a title is present" do
     contribution = mock_model(Contribution)
+    @oral_questions.stub!(:title)
+    @oral_questions.stub!(:introduction)
+    @oral_questions.stub!(:contributions).and_return([contribution])
+    @controller.template.stub!(:render)
+    @controller.template.should_receive(:render).with(:partial => 'contribution', :collection => @oral_questions.contributions)
+    do_render
+  end
+  
+  it "should show the contributions even when a title is present but has no text" do
+    contribution = mock_model(Contribution)
+    @oral_questions.stub!(:title).and_return("")
     @oral_questions.stub!(:introduction)
     @oral_questions.stub!(:contributions).and_return([contribution])
     @controller.template.stub!(:render)
