fd623682bf1edd9c0ca8c9e033a7b143ed67d8a5
Rob McKinnon <rob@theyworkforyou.co.nz>
2007-09-27 17:16:29 +0000

    added get_error_summary method to SourceFile

--- app/models/source_file.rb
+++ app/models/source_file.rb
@@ -8,6 +8,42 @@ class SourceFile < ActiveRecord::Base
     SourceFile.find_or_create_by_name(name)
   end
 
+  def self.get_error_summary
+    files = SourceFile.find(:all).select {|f| !f.log.blank?}
+    error_types = []
+    error_types_to_files = {}
+
+    files.each do |file|
+      file.log.each_line do |error|
+        if error.include? ':'
+          type = error.split(':')[0]
+        else
+          type = error
+        end
+
+        error_types << type unless error_types.include?(type)
+        error_types_to_files[type] = [] unless error_types_to_files.has_key?(type)
+
+        unless error_types_to_files[type].include?(file.name)
+          error_types_to_files[type] << file.name
+        end
+      end
+    end
+
+    error_types.sort! do |a, b|
+      a_size = error_types_to_files[a].size
+      b_size = error_types_to_files[b].size
+      if a_size == b_size
+        a.to_s <=> b.to_s
+      elsif a_size > b_size
+        -1
+      elsif a_size < b_size
+        1
+      end
+    end
+    return error_types, error_types_to_files
+  end
+
   # surely there's a better way of counting lines?
   def log_line_count
     if self.log
@@ -31,7 +67,7 @@ class SourceFile < ActiveRecord::Base
     $stdout.flush
     if persist
       unless self.log.blank?
-        text = "/n" + text
+        text = "\n" + text
       end
       text = self.log + text
       self.log = text
--- spec/models/source_file_spec.rb
+++ spec/models/source_file_spec.rb
@@ -8,6 +8,14 @@ end
 
 describe SourceFile do
 
+  before(:each) do
+    SourceFile.delete_all
+  end
+
+  after(:each) do
+    SourceFile.delete_all
+  end
+
   it "should validate the uniqueness of the source file name" do
     source_file = SourceFile.new(:name => "popular_name")
     source_file.save!
@@ -19,6 +27,35 @@ describe SourceFile do
     source_file = SourceFile.new
     source_file.valid?.should be_true
     source_file.xsd_validated.should be_nil
+  end
+
+  it 'should create error summary hash correctly' do
+
+    source_file_x = SourceFile.new :name => 'x'
+    source_file_x.add_log 'Bad date format: date format="1980-07-28">Monday 22 July 1980'
+    source_file_x.add_log 'Missing column? Got: 593, expected 591 (last column 590)'
+    source_file_x.add_log 'Missing or badly formatted session tag'
+    source_file_x.save!
+    source_file_x.log.should == %Q[Bad date format: date format="1980-07-28">Monday 22 July 1980\nMissing column? Got: 593, expected 591 (last column 590)\nMissing or badly formatted session tag]
+
+    source_file_y = SourceFile.new :name => 'y'
+    source_file_y.add_log 'Missing column? Got: 35, expected 34 (last column 33)'
+    source_file_y.save!
+
+    source_file_z = SourceFile.new :name => 'z'
+    source_file_z.add_log 'Bad date format: date format="1979-11-11">Wednesday 28 November 1979'
+    source_file_z.save!
+
+    error_types, hash = SourceFile.get_error_summary
+    error_types.size.should == 3
+
+    hash['Bad date format'].size.should == 2
+    hash['Missing column? Got'].size.should == 2
+    hash['Missing or badly formatted session tag'].size.should == 1
+
+    error_types[0].should == 'Bad date format'
+    error_types[1].should == 'Missing column? Got'
+    error_types[2].should == 'Missing or badly formatted session tag'
   end
 end
 
