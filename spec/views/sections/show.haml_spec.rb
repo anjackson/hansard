838b20c1221bd918f001da3bee06b190fcc66a02
Robert Brook <mail@robertbrook.com>
2007-10-01 10:33:11 +0000

    moved specs out

--- spec/views/sections/show.haml_spec.rb
+++ spec/views/sections/show.haml_spec.rb
@@ -6,8 +6,10 @@ describe "sections/show.haml", " in general" do
     @section = mock_model(Section)
     @prev = mock_model(Section)
     @prev.stub!(:linkable?)
+    @prev.stub!(:title).and_return("previous section title")
     @next = mock_model(Section)
     @next.stub!(:linkable?)    
+    @next.stub!(:title).and_return("next section title")
     @section.stub!(:title).and_return("section name")
     @controller.template.stub!(:render)
     @section.stub!(:previous_linkable_section).and_return(@prev)
@@ -23,30 +25,6 @@ describe "sections/show.haml", " in general" do
   it "should render the 'section' partial passing the section" do
     @controller.template.should_receive(:render).with(:partial => "section", :locals => {:section => @section})
     do_render
-  end
-
-  it "should not have any text about the previous section if there isn't one" do
-    @section.stub!(:previous_linkable_section).and_return(nil)
-    do_render
-    response.should_not have_tag("a.prev-section")
-  end
-
-  it "should not have any text about the next section if there isn't one" do
-    @section.stub!(:next_linkable_section).and_return(nil)
-    do_render
-    response.should_not have_tag("a.next-section")
-  end
-  
-  it "should have a link to the previous section if there is one and it is linkable" do
-    @controller.template.should_receive(:section_url).with(@prev).and_return("http://www.test-prev.url")      
-    do_render
-    response.should have_tag("a.prev-section[href=http://www.test-prev.url]", :text => "Previous section")
-  end
-    
-  it "should have a link to the next section if there is one and it is linkable" do
-    @controller.template.should_receive(:section_url).with(@next).and_return("http://www.test-next.url")
-    do_render
-    response.should have_tag("a.next-section[href=http://www.test-next.url]", :text => "Next section")
   end
   
 end
