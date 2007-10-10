4da8179723fa09a1e2ab7cdfd1ea4cc61ac70b8a
Rob McKinnon <rob@theyworkforyou.co.nz>
2007-10-10 14:11:46 +0000

    maps lords to correct sitting model

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,13 +1,26 @@
 class SectionsController < ApplicationController
-  
+
   before_filter :check_valid_date, :only => [:show]
-  
+
   def show
-    @marker_options = {}
-    types_to_sittings = { "commons"        => HouseOfCommonsSitting,
-                          "writtenanswers" => WrittenAnswersSitting }
-    @sitting = types_to_sittings[params[:type]].find_by_date(@date.to_date.to_s)
+    sitting_model = types_to_sittings(params[:type])
+
+    @sitting = sitting_model.find_by_date(@date.to_date.to_s)
     @section = @sitting.sections.find_by_slug(params[:id])
+    @marker_options = {}
   end
+
+  private
+
+    def types_to_sittings type
+      case type
+        when "commons"
+          HouseOfCommonsSitting
+        when "lords"
+          HouseOfLordsSitting
+        when "writtenanswers"
+          WrittenAnswersSitting
+      end
+    end
 
 end
