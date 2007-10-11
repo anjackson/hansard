b9402dab1b681a2bbddaaa1ad724fc650f6a30af
Rob McKinnon <rob@theyworkforyou.co.nz>
2007-10-11 11:01:53 +0000

    display indices grouped by decade in indices index view

--- spec/views/indices/index_spec.rb
+++ spec/views/indices/index_spec.rb
@@ -5,17 +5,18 @@ describe "index", " in general" do
   before do
     @index = mock_model(Index)
     @index.stub!(:title)
-    assigns[:indices] = [@index]
+    @index.stub!(:decade).and_return(1920)
+    assigns[:indices_by_decade] = [[@index]]
   end
-  
+
   it "should have an 'h1' tag containing the text 'Historical Hansard: Indices'" do
-    @controller.stub_render(:partial => 'index', :collection => [@index]) 
+    @controller.stub_render(:partial => 'index', :collection => [@index])
     render 'indices/index.haml'
-    response.should have_tag("h1", :text => "Historical Hansard: Indices")
+    response.should have_tag("h1", :text => "Hansard: Indices")
   end
-   
+
   it "should render the index partial for each index" do
-    @controller.expect_render(:partial => 'index', :collection => [@index]) 
+    @controller.expect_render(:partial => 'index', :collection => [@index])
     render 'indices/index.haml'
   end
 
