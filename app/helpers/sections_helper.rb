96e74e9aca067bdc40a4bd5ac5a3f3ac36725071
Robert Brook <mail@robertbrook.com>
2007-10-16 16:53:29 +0000

    did I break this?

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -5,15 +5,13 @@ module SectionsHelper
   
     if section.previous_linkable_section
       the_links << "<p><ol>"
-      the_links << "<li>&larr; Previously</li>"
-      the_links << "<li><a class='prev-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></li>"
+      the_links << "<li>&larr; <a class='prev-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></li>"
       the_links << "</ol></p>"
     end
   
     if section.next_linkable_section
       the_links << "<p><ol>"
-      the_links << "<li>Next &rarr;</li>"
-      the_links << "<li><a class='next-section' href='#{section_url(section.next_linkable_section)}'>#{section.next_linkable_section.title}</a></li>"
+      the_links << "<li><a class='next-section' href='#{section_url(section.next_linkable_section)}'>#{section.next_linkable_section.title}</a> &rarr;</li>"
       the_links << "</ol></p>"
     end
   
