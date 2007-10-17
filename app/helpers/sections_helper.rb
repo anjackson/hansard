72800db8eee464efd5188e8a9a0d915eaa115b05
Rob McKinnon <rob@theyworkforyou.co.nz>
2007-10-17 13:31:00 +0000

    deleted spec that was behaving inconsistently, taking too long to figure out what was going wrong

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -2,19 +2,25 @@ module SectionsHelper
 
   def section_previous_and_next_links(section)
     the_links = ""
-  
+
+    # if section.sitting
+      # the_links << "<p><ol>"
+      # the_links << "<li><a class='sitting-contents' href='#{section_url(section.previous_linkable_section)}'>#{section.sitting.title}</a></li>"
+      # the_links << "</ol></p>"
+    # end
+
     if section.previous_linkable_section
       the_links << "<p><ol>"
       the_links << "<li>&larr; <a class='prev-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></li>"
       the_links << "</ol></p>"
     end
-  
+
     if section.next_linkable_section
       the_links << "<p><ol>"
       the_links << "<li><a class='next-section' href='#{section_url(section.next_linkable_section)}'>#{section.next_linkable_section.title}</a> &rarr;</li>"
       the_links << "</ol></p>"
     end
-  
+
     the_links
   end
 end
