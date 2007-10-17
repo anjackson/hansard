61a2b178ed13a0306910dc33e44f1576b46f49e5
Louise Crow <louise.crow@gmail.com>
2007-10-17 14:33:03 +0000

    Adding nav link for sections up to the sitting

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,6 +1,6 @@
 module SectionsHelper
-
-  def section_previous_and_next_links(section)
+  
+  def section_nav_links(section)
     the_links = ""
 
     # if section.sitting
@@ -9,6 +9,10 @@ module SectionsHelper
       # the_links << "</ol></p>"
     # end
 
+    the_links << "<p><ol>"
+    the_links << "<li>&uarr; <a class='section-sitting' href='#{sitting_date_url(section.sitting)}'>#{section.sitting.title} #{section.sitting.date_text}</a></li>"
+    the_links << "</ol></p>"
+    
     if section.previous_linkable_section
       the_links << "<p><ol>"
       the_links << "<li>&larr; <a class='prev-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></li>"
--- spec/helpers/sections_helper_spec.rb
+++ spec/helpers/sections_helper_spec.rb
@@ -1,11 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 
-describe SectionsHelper, " when using section_previous_and_next_links to create links for a section" do
+describe SectionsHelper, " when using section_nav_links to create links for a section" do
   
   before do
     stub!(:section_url).and_return("http://www.test.url")
+    stub!(:sitting_date_url).and_return("http://www.test.url")
     @section = mock_model(Section)
+    @sitting = mock_model(Sitting)
+    @sitting.stub!(:title).and_return("sitting title")
+    @sitting.stub!(:date_text).and_return("sitting date text")
+    @section.stub!(:sitting).and_return(@sitting)
     @prev = mock_model(Section)
     @prev.stub!(:linkable?)
     @prev.stub!(:title).and_return("previous section title")
@@ -18,24 +23,29 @@ describe SectionsHelper, " when using section_previous_and_next_links to create 
     assigns[:section] = @section
   end
    
+   
+  it "should have a link to the sitting showing the sitting's title and date text" do
+    section_nav_links(@section).should have_tag("a.section-sitting", :text => "sitting title sitting date text")
+  end
+  
   it "should not have any text about the previous linkable section if there isn't one" do
     @section.stub!(:previous_linkable_section).and_return(nil)
-    section_previous_and_next_links(@section).should_not have_tag("a.prev-section")
+    section_nav_links(@section).should_not have_tag("a.prev-section")
   end
   
   it "should not have any text about the next section if there isn't one" do
     @section.stub!(:next_linkable_section).and_return(nil)
-    section_previous_and_next_links(@section).should_not have_tag("a.next-section")
+    section_nav_links(@section).should_not have_tag("a.next-section")
   end
      
   it "should have a link to the previous section if there is one and it is linkable" do
     should_receive(:section_url).with(@prev).and_return("http://www.test-prev.url")      
-    section_previous_and_next_links(@section).should have_tag("a.prev-section[href=http://www.test-prev.url]", :text => "previous section title")
+    section_nav_links(@section).should have_tag("a.prev-section[href=http://www.test-prev.url]", :text => "previous section title")
   end
   
   it "should have a link to the next section if there is one and it is linkable" do 
     should_receive(:section_url).with(@next).and_return("http://www.test-next.url")      
-    section_previous_and_next_links(@section).should have_tag("a.next-section[href=http://www.test-next.url]", :text => "next section title")
+    section_nav_links(@section).should have_tag("a.next-section[href=http://www.test-next.url]", :text => "next section title")
   end
   
 end
