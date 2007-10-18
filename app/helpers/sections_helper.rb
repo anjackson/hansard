c1bf5125ff86e633d2ca2e1f5a84b683583ab599
Robert Brook <mail@robertbrook.com>
2007-10-18 13:33:11 +0000

    slimming

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -9,21 +9,18 @@ module SectionsHelper
       # the_links << "</ol></p>"
     # end
 
-    the_links << "<p><ol>"
-    the_links << "<li>&uarr; <a class='section-sitting' href='#{sitting_date_url(section.sitting)}'>#{section.sitting.title} #{section.sitting.date_text}</a></li>"
-    the_links << "</ol></p>"
+    the_links << "<ol id='arrow-navigation'>"
+    the_links << "<li>&uarr; Up to <a class='section-sitting' href='#{sitting_date_url(section.sitting)}'>#{section.sitting.title} #{section.sitting.date_text}</a></li>"
     
     if section.previous_linkable_section
-      the_links << "<p><ol>"
-      the_links << "<li>&larr; <a class='prev-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></li>"
-      the_links << "</ol></p>"
+      the_links << "<li>&larr; Back to <a class='prev-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></li>"
     end
 
     if section.next_linkable_section
-      the_links << "<p><ol>"
-      the_links << "<li><a class='next-section' href='#{section_url(section.next_linkable_section)}'>#{section.next_linkable_section.title}</a> &rarr;</li>"
-      the_links << "</ol></p>"
+      the_links << "<li>On to <a class='next-section' href='#{section_url(section.next_linkable_section)}'>#{section.next_linkable_section.title}</a> &rarr;</li>"
     end
+
+    the_links << "</ol>"
 
     the_links
   end
