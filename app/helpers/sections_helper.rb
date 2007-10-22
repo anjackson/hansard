b706681b4f4aa8894d5a4bd0d0a5d80c51b97d5d
Robert Brook <mail@robertbrook.com>
2007-10-22 16:15:21 +0000

    ids

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -13,11 +13,11 @@ module SectionsHelper
   end
   
   def section_nav_links(section)
-    the_links = "<p>"
-    the_links << "<p>Parent section:<br/> <a class='section-sitting' href='#{sitting_date_url(section.sitting)}'>#{section.sitting.title} #{section.sitting.date_text}</a></p>"
-    the_links << "<p>Previous section:<br/> <a class='previous-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></p>" if section.previous_linkable_section
-    the_links << "<p>Next section:<br/> <a class='next-section' href='#{section_url(section.next_linkable_section)}'>#{section.next_linkable_section.title}</a></p>" if section.next_linkable_section
-    the_links << "</p>"
+    the_links = "<div id='navigation-by-sections'>"
+    the_links << "<p id='parent-section'>Parent section<br/> <a class='section-sitting' href='#{sitting_date_url(section.sitting)}'>#{section.sitting.title} #{section.sitting.date_text}</a></p>"
+    the_links << "<p id='previous-section'>Previous section<br/> <a class='previous-section' href='#{section_url(section.previous_linkable_section)}'>#{section.previous_linkable_section.title}</a></p>" if section.previous_linkable_section
+    the_links << "<p id='next-section'>Next section<br/> <a class='next-section' href='#{section_url(section.next_linkable_section)}'>#{section.next_linkable_section.title}</a></p>" if section.next_linkable_section
+    the_links << "</div>"
     the_links
   end
 end
