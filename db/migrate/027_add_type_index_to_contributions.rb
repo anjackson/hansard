94bfc7fe07204d635ec100151ab604a5fa3e2c78
Louise Crow <louise.crow@gmail.com>
2007-10-22 15:28:15 +0000

    Removing type migration

--- db/migrate/027_add_type_index_to_contributions.rb
+++
@@ -1,9 +0,0 @@
-class AddTypeIndexToContributions < ActiveRecord::Migration
-  def self.up
-    add_index :contributions, :type
-  end
-
-  def self.down
-    remove_index :contributions, :type
-  end
-end
