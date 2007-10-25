4ad98bc0bf0a692b41ca7692b90d0aa55a6efa7e
Rob McKinnon <rob@theyworkforyou.co.nz>
2007-10-25 16:39:07 +0000

    added source_file_id

--- db/migrate/030_create_parliament_sessions.rb
+++ db/migrate/030_create_parliament_sessions.rb
@@ -2,22 +2,23 @@ class CreateParliamentSessions < ActiveRecord::Migration
   def self.up
     drop_table :sessions
     create_table :parliament_sessions do |t|
-      t.string :series_number
-      t.string :volume_in_series
-      t.string :type
-      t.string :session_of_parliament
-      t.string :number_of_parliament
-      t.string :year_of_the_reign
-      t.string :monarch_name
-      t.string :years_of_session
-      t.string :volume_of_session
-      t.string :period_start_date_text
-      t.string :period_end_date_text
-      t.date   :period_start_date
-      t.date   :period_end_date
-      t.string :may_be_cited_as
-      t.string :isbn
-      t.text   :titlepage_text
+      t.string  :series_number
+      t.string  :volume_in_series
+      t.string  :type
+      t.integer :source_file_id
+      t.string  :session_of_parliament
+      t.string  :number_of_parliament
+      t.string  :year_of_the_reign
+      t.string  :monarch_name
+      t.string  :years_of_session
+      t.string  :volume_of_session
+      t.string  :period_start_date_text
+      t.string  :period_end_date_text
+      t.date    :period_start_date
+      t.date    :period_end_date
+      t.string  :may_be_cited_as
+      t.string  :isbn
+      t.text    :titlepage_text
     end
   end
 
