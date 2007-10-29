0b03d5186873e8540c08ee645ae56d4a85a8913d
Louise Crow <louise.crow@gmail.com>
2007-10-29 17:53:23 +0000

    Rewrite to avoid bad input tag closures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -219,8 +219,8 @@ module ApplicationHelper
 
   def search_form id='search', submit_text='Search', &block
     open :form, { :action => "#{search_url}", :id => id } do
-      open :input, { :name => 'query', :type => 'text', :size => '20', :accesskey => 's', :value => @query}
-      open :input, { :name => 'sa', :type => 'submit', :value => submit_text}
+      puts "<input accesskey='s' name='query' size='20' type='text' value='test'>"
+      puts "<input name='sa' type='submit' value='#{submit_text}'>"
       yield if block
     end
   end
