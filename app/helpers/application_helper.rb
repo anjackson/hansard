d7cb887bed1968426c83d0f4370578aa62441d9c
Robert Brook <mail@robertbrook.com>
2007-10-29 12:19:43 +0000

    sessions link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -188,11 +188,11 @@ module ApplicationHelper
           end
         end
         
-        # open :li do
-        #   open :a, { :href => members_url } do
-        #     puts "Members"
-        #   end
-        # end
+        open :li do
+                  open :a, { :href => parliament_sessions_url } do
+                    puts "Sessions"
+                  end
+                end
 
         open :li do
           open :a, { :href => source_files_url } do
